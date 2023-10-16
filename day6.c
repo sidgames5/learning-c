b7b9f692f97f712771a124624ae914d48637fcad
SidGames5 <saytleader@outlook.com>
2023-10-16 16:05:30 -0400

    day 6

---
+++ day6.c
@@ -0,0 +1,13 @@
+// https://youtu.be/KJgsSFOSQv0?t=5189
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int luckyNumbers[] = {4, 8, 16, 32, 64, 128, 256};
+    luckyNumbers[1] = 5;
+    printf("%d", luckyNumbers[1]);
+
+    return 0;
+}
