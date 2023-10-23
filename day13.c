8e478338d24adf2476402c1aa8b172eac4f7458d
SidGames5 <saytleader@outlook.com>
2023-10-23 18:18:00 -0400

    day 13

---
+++ day13.c
@@ -0,0 +1,16 @@
+// https://youtu.be/KJgsSFOSQv0?t=10745
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int luckyNumbers[] = {4, 8, 15, 16, 23, 42};
+
+    for (int i = 0; i <= 5; i++)
+    {
+        printf("%d\n", luckyNumbers[i]);
+    }
+
+    return 0;
+}
